a44571c51279d31ad60c554d021ad21dfa5a318a
zacharyjxy2020 <42543859+zacharyjxy2020@users.noreply.github.com>
2020-10-24 02:05:38 +0800

    second commit

--- RobloxInputSite/src/components/App.js
+++ RobloxInputSite/src/components/App.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import { Route } from 'react-router';
 
 
 class App extends Component {
     render() {
         return (
-                <div>
-                   Hello, World
-                </div>
-            
+            <Router>
+                    <div>
+                        <Switch>
+                            <Route path="/homepage" exact component={homepage}/>
+                        </Switch>
+                    </div>
+                </Router>            
         )
     }
 }
+
+ReactDOM.render(<App/>, document.getElementById('app'));
---
+++ RobloxInputSite/src/components/homepage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class homepage extends Component {
+    render(){
+        return(
+            <div>
+                Hello World
+            </div>
+        );
+    }
+}
